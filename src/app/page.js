f3ae7a29fd95cde3c6521173b40bc0fd50aedaf6
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-06-07 05:55:25 +0100

    feat: import Nav component into homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import Image from 'next/image'
+import Nav from "./component/Nav";
+
 
 export default function Home() {
   return (
   <>
-  <div className="flex min-h-screen flex-col items-center justify-between p-2">
-hiiii
+  <div className='homepage'>
+    <div className="">
+      <Nav/>
+    </div>
   </div>
   </>
   )
